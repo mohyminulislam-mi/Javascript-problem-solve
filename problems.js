064d64adf590389313d5d8299abef448e9526b7c
Mohyminul Islam <mohyminulmunna23@gmail.com>
2026-01-19 11:55:03 +0600

    added more five js problems

--- problems.js
+++ problems.js
@@ -38,3 +38,58 @@ const removeDuplicates = (arr) => {
 };
 
 console.log(removeDuplicates([1, 2, 2, 3, 4, 4]));
+
+// Problem 6: Sum of All Numbers in an Array
+const sumArray = (arr) => {
+  let sum = 0;
+  for (let num of arr) {
+    sum += num;
+  }
+  return sum;
+};
+console.log(sumArray([1, 2, 3, 4]));
+
+// Problem 7: Find Even Numbers in an Array
+const findEvenNumbers = (arr) => {
+  return arr.filter((num) => num % 2 === 0);
+};
+
+console.log(findEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+// Problem 8: Capitalize First Letter of Each Word
+const capitalizeWords = (str) => {
+  return str
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+};
+
+console.log(capitalizeWords("hello world"));
+
+// Problem 9: Find the Factorial of a Number
+const factorial = (n) => {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+};
+
+console.log(factorial(5));
+
+// Problem 10: PingPong Challenge
+const pingPong = () => {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("PingPong");
+    } else if (i % 3 === 0) {
+      console.log("Ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+};
+
+pingPong();
